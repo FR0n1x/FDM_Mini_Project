3389fc4ee314d4b0ca677ad6bc7789278f23aef4
FR0n1x <105339444+FR0n1x@users.noreply.github.com>
2024-10-03 14:04:12 +0530

    Updated the python file

--- Models/RandomForest/RailGuard(Railway Accident Prediction System).py
+++ Models/RandomForest/RailGuard(Railway Accident Prediction System).py
@@ -27,7 +27,7 @@ def accident_prediction(input_data):
     
 def main():
     # giving the title
-    st.title("Railway Accident Prediction Web App")
+    st.title("RailGuard")
     
     # getting the input data from the user
     Track_Class = st.text_input("Track Class")
@@ -36,7 +36,6 @@ def main():
     Train_Speed = st.text_input("Train Speed")
     Joint_Track_Class = st.text_input("Joint Track Class")
     Track_Type_Code = st.text_input("Track Type Code")
-    Class_ = st.text_input("Class")
     Station_encoded = st.text_input("Station (Encoded)")
     Track_Name_encoded = st.text_input("Track Name (Encoded)")
     
@@ -45,7 +44,7 @@ def main():
     # creating a button for the prediction
     if st.button('Accident Prediction Result'):
         result = accident_prediction([Track_Class, Gross_Tonnage, Maximum_Speed, Train_Speed, 
-                                         Joint_Track_Class, Track_Type_Code, Class_, Station_encoded, Track_Name_encoded])
+                                         Joint_Track_Class, Track_Type_Code, Station_encoded, Track_Name_encoded])
         
     st.success(result)
     
